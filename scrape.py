5c9c65b5590a77e59fa184e92b51a9110f2bb3db
rahulsingh34 <rahulsingh342000@gmail.com>
2023-05-08 19:16:19 -0400

    added more fields to pull

--- scrape.py
+++ scrape.py
@@ -4,14 +4,22 @@ import pandas as pd
 url_a = "https://ratethelandlord.org/api/get-reviews?page="
 url_b = "&sort=new&state=ONTARIO&country&city&zip&search&limit="
 
-df = pd.DataFrame(columns = ["Landlord", "City", "Review"])
+df = pd.DataFrame(columns = ["Landlord", "City", "Zip", "Review", "Repair", "Health", "Stability", "Privacy", "Respect"])
 
 for i in range(1, 100):
     try:
         response = requests.request("GET", url_a + str(i) + url_b)
         response = response.json()
         for i in range(len(response["reviews"])):
-            df.loc[len(df.index)] = [response["reviews"][i]["landlord"], response["reviews"][i]["city"], response["reviews"][i]["review"]] 
+            df.loc[len(df.index)] = [response["reviews"][i]["landlord"],
+                                      response["reviews"][i]["city"],
+                                      response["reviews"][i]["zip"],
+                                      response["reviews"][i]["review"],
+                                      response["reviews"][i]["repair"],
+                                      response["reviews"][i]["health"],
+                                      response["reviews"][i]["stability"],
+                                      response["reviews"][i]["privacy"],
+                                      response["reviews"][i]["respect"]]
     except:
         print("error")
 
