dd18a6f21eab3f695a960142275dcf4396c80286
rahulsingh34 <rahulsingh342000@gmail.com>
2023-05-15 21:21:50 -0400

    full site scrape

--- scrape.py
+++ scrape.py
@@ -1,18 +1,21 @@
 import requests
 import pandas as pd
 
+print("running")
+
 url_a = "https://ratethelandlord.org/api/get-reviews?page="
-url_b = "&sort=new&state=ONTARIO&country&city&zip&search&limit="
+url_b = "&sort=new&state=&country&city&zip&search&limit="
 
-df = pd.DataFrame(columns = ["Landlord", "City", "Zip", "Review", "Repair", "Health", "Stability", "Privacy", "Respect"])
+df = pd.DataFrame(columns = ["Landlord", "City", "State", "Zip", "Review", "Repair", "Health", "Stability", "Privacy", "Respect"])
 
-for i in range(1, 100):
+for i in range(1, 500):
     try:
         response = requests.request("GET", url_a + str(i) + url_b)
         response = response.json()
         for i in range(len(response["reviews"])):
             df.loc[len(df.index)] = [response["reviews"][i]["landlord"],
                                       response["reviews"][i]["city"],
+                                      response["reviews"][i]["state"],
                                       response["reviews"][i]["zip"],
                                       response["reviews"][i]["review"],
                                       response["reviews"][i]["repair"],
