41d389cfd1ac9418ff1e448942e8a4a22c352cb5
rahulsingh34 <rahulsingh342000@gmail.com>
2023-05-16 13:20:59 -0400

    country code

--- scrape.py
+++ scrape.py
@@ -6,9 +6,9 @@ print("running")
 url_a = "https://ratethelandlord.org/api/get-reviews?page="
 url_b = "&sort=new&state=&country&city&zip&search&limit="
 
-df = pd.DataFrame(columns = ["Landlord", "City", "State", "Zip", "Review", "Repair", "Health", "Stability", "Privacy", "Respect"])
+df = pd.DataFrame(columns = ["Landlord", "City", "State", "Zip", "Country", "Review", "Repair", "Health", "Stability", "Privacy", "Respect"])
 
-for i in range(1, 500):
+for i in range(1, 350):
     try:
         response = requests.request("GET", url_a + str(i) + url_b)
         response = response.json()
@@ -17,6 +17,7 @@ for i in range(1, 500):
                                       response["reviews"][i]["city"],
                                       response["reviews"][i]["state"],
                                       response["reviews"][i]["zip"],
+                                      response["reviews"][i]["country_code"],
                                       response["reviews"][i]["review"],
                                       response["reviews"][i]["repair"],
                                       response["reviews"][i]["health"],
